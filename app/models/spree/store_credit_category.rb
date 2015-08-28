764d554753f6b0eeabfcc372f8c2ae15eac819d9
Jorgen Johnson <jorgenj@gmail.com>
2015-08-27 21:39:06 -0700

    Migrations don't work if we add validations on columns that have yet to be created.

--- app/models/spree/store_credit_category.rb
+++ app/models/spree/store_credit_category.rb
@@ -4,7 +4,7 @@ class Spree::StoreCreditCategory < ActiveRecord::Base
   belongs_to :credit_type, class_name: 'Spree::StoreCreditType', :foreign_key => 'type_id'
   delegate :non_expiring?, to: :credit_type
 
-  validates_presence_of :type_id
+  # validates_presence_of :type_id
   validates_presence_of :name
   validates_uniqueness_of :name
 end
